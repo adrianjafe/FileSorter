e1536bb1c83d225212514af8888ebb3ef8e77816
adrianjafe <145451036+adrianjafe@users.noreply.github.com>
2025-10-22 20:10:33 +0200

    Update FileScanner

--- src/FileScanner.java
+++ src/FileScanner.java
@@ -1,4 +1,5 @@
 package src;
+
 import java.io.File;
 import java.nio.file.Files;
 import org.json.simple.JSONObject;
@@ -6,22 +7,24 @@ import org.json.simple.JSONArray;
 import org.json.simple.parser.JSONParser;
 import java.io.FileReader;
 
-
 public class FileScanner {
 
-    //-------------------------------------------------Main-----------------------------------------------------------//
+    // -------------------------------------------------Main-----------------------------------------------------------//
     public static void main(String[] args) {
-        String path = "./prueba";
+        String path = "./src/prueba"; //Ruta que se puede cambiar desde la app.
         File folder = getFile(path);
-        for( File file : folder.listFiles()){
-            System.out.println(file.getName() + " - " + file.length()/1024 + " KB");
+        System.out.println(folder.isDirectory());
+        for (File file : folder.listFiles()) {
+            System.out.println(file.getName() + " - " + file.length() / 1024 + " KB");
         }
+        
+        organizarPorTipo(folder);
     }
 
-    public JSONArray leerJSON(String tipo){
-        try{
+    public static JSONArray leerJSON(String tipo) {
+        try {
             JSONParser parser = new JSONParser();
-            JSONObject jsonObject = (JSONObject) parser.parse(new FileReader("./res/extensiones.json"));
+            JSONObject jsonObject = (JSONObject) parser.parse(new FileReader("./src/res/extensiones.json"));
             JSONArray lista = (JSONArray) jsonObject.get(tipo);
             return lista;
         } catch (Exception e) {
@@ -30,89 +33,98 @@ public class FileScanner {
         }
     }
 
-    public static File getFile(String path){
+    public static File getFile(String path) {
+        //File folder = new File(path);
+        System.out.println("Obteniendo archivo o carpeta en: " + new File(path).isDirectory());
         return new File(path);
     }
 
-    //Método para mover archivos a una carpeta de destino
-    public void moverArchivo(File file, String destino){
+    // Método para mover archivos a una carpeta de destino
+    public static void moverArchivo(File file, String destino) {
         File destinoFolder = getFile(destino);
-        if(destinoFolder.exists() == false){
+        if (destinoFolder.exists() == false) {
             destinoFolder.mkdirs();
         }
-
-        try{
-            Files.move(file.toPath(), destinoFolder.toPath());
+        System.out.println("Moviendo " + file + " a " + destinoFolder);
+        try {
+            Files.move(file.toPath(), destinoFolder.toPath().resolve(file.getName()));
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-      //----------------------------- Metodos de organización -------------------------------//
-     //                                                                                     //
-    //              Método para organizar archivos por tipo                                //    
-    public void organizarPorTipo(String path, String destino){
-        File folder = getFile(path);
-        for( File file : folder.listFiles()){
-            if(isImagen(file)){
-                moverArchivo(file, destino);
+    // ----------------------------- Metodos de organización
+    // -------------------------------//
+    // //
+    // Método para organizar archivos por tipo //
+    public static void organizarPorTipo(File folder) {
+        for (File file : folder.listFiles()) {
+            if (isImagen(file)) {
+                moverArchivo(file, "./src/Imagen");
+            } else if (isAudio(file)) {
+                moverArchivo(file, "./src/Audio");
+            } else if (isVideo(file)) {
+                moverArchivo(file, "./src/Video");
+            } else if (isDocumento(file)) {
+                moverArchivo(file, "./src/Documento");
+                System.out.println("Documento: " + file.getName() + " movido.");
             }
         }
     }
 
-   //                                                                                 //
-  //---------------------------------------------------------------------------------//
+    // //
+    // ---------------------------------------------------------------------------------//
 
-    
-      //----------------------------- Metodos de verificación -------------------------------//
-     //                                                                                     //
-    //              Método para verificar si un archivo es una imagen    
-    
-                //To Do: Hacer JSON con extensiones y que lo recorra 
-    public boolean isImagen(File file){
+    // ----------------------------- Metodos de verificación
+    // -------------------------------//
+    // //
+    // Método para verificar si un archivo es una imagen
+
+    // To Do: Hacer JSON con extensiones y que lo recorra
+    public static boolean isImagen(File file) {
         String name = file.getName().toLowerCase();
         JSONArray extension = leerJSON("Imagen");
-        for(Object obj : extension){
-            if(name.endsWith((String) obj)){
+        for (Object obj : extension) {
+            if (name.endsWith((String) obj)) {
                 return true;
             }
         }
         return false;
     }
 
-    public boolean isDocumento(File file){
+    public static boolean isDocumento(File file) {
         String name = file.getName().toLowerCase();
         JSONArray extension = leerJSON("Documento");
-        for(Object obj : extension){
-            if(name.endsWith((String) obj)){
+        for (Object obj : extension) {
+            if (name.endsWith((String) obj)) {
                 return true;
             }
         }
         return false;
     }
 
-    public boolean isVideo(File file){
+    public static boolean isVideo(File file) {
         String name = file.getName().toLowerCase();
         JSONArray extension = leerJSON("Video");
-        for(Object obj : extension){
-            if(name.endsWith((String) obj)){
+        for (Object obj : extension) {
+            if (name.endsWith((String) obj)) {
                 return true;
             }
         }
         return false;
     }
 
-    public boolean isAudio(File file){
+    public static boolean isAudio(File file) {
         String name = file.getName().toLowerCase();
         JSONArray extension = leerJSON("Audio");
-        for(Object obj : extension){
-            if(name.endsWith((String) obj)){
+        for (Object obj : extension) {
+            if (name.endsWith((String) obj)) {
                 return true;
             }
         }
         return false;
     }
 
-   //                                                                                 //
-  //---------------------------------------------------------------------------------//
+    // //
+    // ---------------------------------------------------------------------------------//
 }
